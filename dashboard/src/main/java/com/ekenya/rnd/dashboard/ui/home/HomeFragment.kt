35dbbba099ae0378a3153d6333a0dea235bbb8bb
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2023-09-19 22:27:08 +0300

    search view

--- dashboard/src/main/java/com/ekenya/rnd/dashboard/ui/home/HomeFragment.kt
+++ dashboard/src/main/java/com/ekenya/rnd/dashboard/ui/home/HomeFragment.kt
@@ -5,10 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ekenya.rnd.common.abstractions.BaseDaggerFragment
+import com.ekenya.rnd.common.model.ShipResponseItem
 import com.ekenya.rnd.common.utils.Status
 import com.ekenya.rnd.dashboard.MainActivity
 import com.ekenya.rnd.dashboard.R
@@ -21,6 +23,8 @@ class HomeFragment : BaseDaggerFragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var shipAdapter: ShipAdapter
+
+    var filteredShips: List<ShipResponseItem> = ArrayList()
 
     @Inject
     lateinit var factory: ViewModelProvider.Factory
@@ -48,6 +52,36 @@ class HomeFragment : BaseDaggerFragment() {
 
         observeShips()
 
+        //perfoming search
+        val searchView = binding.searchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                // Perform filtering or live search as the user types
+                filterShip(newText)
+                return true
+            }
+        })
+
+    }
+
+    private fun filterShip(ship: String) {
+        val filteredList =
+            filteredShips.filter { it.ship_name?.contains(ship, ignoreCase = true) ?: false }
+        val theFilteredShips = ArrayList(filteredList)
+
+        if (theFilteredShips.isEmpty()){
+           // binding.errorLayout.visibility = View.VISIBLE
+        } else {
+           // binding.errorLayout.visibility = View.GONE
+        }
+
+        shipAdapter = ShipAdapter(theFilteredShips)
+        binding.recyclerView.adapter = shipAdapter
+        shipAdapter.notifyDataSetChanged()
     }
 
 
@@ -68,6 +102,7 @@ class HomeFragment : BaseDaggerFragment() {
                     Log.d("kim", "observeParking: ${shipResponse.data}")
 
                     ship?.let {
+                        filteredShips = ship
                         shipAdapter = ShipAdapter(it)
                         setRecyclerView()
                     }
