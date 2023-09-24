71c4bcb0addc75422ed2687f4ab981965a97cdf5
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2023-09-24 08:59:00 +0300

    passing ship details in shipdetails fragment

--- dashboard/src/main/java/com/ekenya/rnd/dashboard/ui/home/ShipDetailsFragment.kt
+++ dashboard/src/main/java/com/ekenya/rnd/dashboard/ui/home/ShipDetailsFragment.kt
@@ -1,15 +1,20 @@
 package com.ekenya.rnd.dashboard.ui.home
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.navArgs
 import com.ekenya.rnd.common.abstractions.BaseDaggerFragment
 import com.ekenya.rnd.common.model.ShipResponseItem
 import com.ekenya.rnd.common.utils.Status
+import com.ekenya.rnd.dashboard.R
 import com.ekenya.rnd.dashboard.databinding.FragmentShipDetailsBinding
 import com.squareup.picasso.Picasso
 import javax.inject.Inject
@@ -37,6 +42,14 @@ class ShipDetailsFragment : BaseDaggerFragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentShipDetailsBinding.inflate(inflater, container, false)
+
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        // Enable the back arrow in the toolbar
+        (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Set the click listener for the back arrow
+        binding.toolbar.setNavigationOnClickListener {
+            NavHostFragment.findNavController(this).popBackStack()
+        }
         return binding?.root
     }
 
@@ -50,50 +63,32 @@ class ShipDetailsFragment : BaseDaggerFragment() {
     private fun observeShipDetails() {
         val ship = requireArguments().getParcelable<ShipResponseItem>("item")
         ship?.let {
+            binding.progressBar2.visibility = View.VISIBLE
             Picasso.get().load(ship.image).into(binding.shipImg)
-            binding.shipTitleTv.text = ship.ship_name
+            if (ship.image != null){
+                Picasso.get().load(ship.image).into(binding.shipImg)
+            }else{
+                binding.shipImg.setImageResource(R.drawable.ship)
+            }
+
+            //ship title with underline
+            val shipName = ship.ship_name
+            val spannableString = SpannableString(shipName)
+            spannableString.setSpan(UnderlineSpan(), 0, spannableString.length, 0)
+
+            binding.shipTitleTv.text = spannableString
             binding.shipModelTv.text = ship.ship_type ?: "0"
             binding.shipSpeedTv.text = ship.speed_kn?.toString() ?: "0"
-            // binding.shipSummaryTv.text = ship.
-
             binding.shipTypeTv.text = ship.ship_type?.toString() ?: "0"
 
             binding.shipWeightTv.text = ship.weight_kg?.toString() ?: "0"
 
+            binding.progressBar2.visibility = View.GONE
+
         }
     }
 
-//    private fun observeShipDetails() {
-//        viewModel.getShips()
-//        viewModel.observeShipLiveData().observe(
-//            viewLifecycleOwner
-//        ){
-//            ships ->
-//            when(ships.status){
-//                Status.SUCCESS ->{
-//                    val response = ships.data
-//
-//                    response.let {
-//                        var response = ships.data?.find { ship ->
-//                            ship.ship_id == shipId
-//                        }
-//
-//                        binding.shipTitleTv.text = response?.ship_name
-//                        Picasso.get().load(response?.image).into(binding.shipImg)
-//                        Log.d("main","iamge ${response?.image}")
-//                    }
-//                }
-//                Status.LOADING -> {
-//
-//                }
-//                Status.ERROR ->{
-//
-//                }
-//            }
-//
-//
-//        }
-//    }
+
 
 
 }
